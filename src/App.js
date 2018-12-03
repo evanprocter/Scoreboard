789be50205b2aec0bb305d0137aaee7b60b9768e
Evan Procter <evanprocter@gmail.com>
2018-12-03 11:40:42 -0500

    added implementation of score when plus button is clicked

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -42,14 +41,59 @@ class App extends Component {
   _scoresAsCards(){
     const cards = this.state.scores.map(score => {
       return (
-        <div>
+        <div key={score.id}>
           <h2>
-            Name: {score.name} // Score: {score.score}</h2>
+            Name: {score.name} // Score: {score.score}
+          </h2>
+          <button onClick={() => this._incrementScoreById(score.id)}>+</button>
         </div>
-      )
+      );
     });
     return cards;
   }
+
+  // version 1: .map manually constructing replacement
+  // _incrementScoreById(id){
+  //   // find the player in this.state.scores
+  //   // increment their score
+  //   const newScores = this.state.scores.map(score => {
+  //     if(score.id !== id){
+  //       return score
+  //     }else{
+  //       return {
+  //         id: score.id,
+  //         name: score.name,
+  //         score: score.score + 1
+  //       }
+  //     }
+  //   })
+  //   // and call this.setState
+  //   this.setState({
+  //     scores: newScores
+  //   });
+  // }
+
+  // version 2: .map, using a shorthand to copy values out of the original
+_incrementScoreById(id){
+  // find the player in this.state.scores
+  // increment their score
+  const newScores = this.state.scores.map(bruce => {
+    if(bruce.id !== id){
+      return bruce
+    }else{
+      return {
+        ...bruce,
+        score: bruce.score + 1
+      };
+    }
+  });
+
+  // and call this.setState
+  this.setState({
+
+})
+}
+
 }
 
 export default App;
